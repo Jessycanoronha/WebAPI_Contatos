0928e4f0ae3e70441d1f59d84c797e9d160d677d
Jéssyca <jessycarozeene@gmail.com>
2019-06-07 10:10:37 -0300

    Add files via upload

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,9 @@
+using WebApi2_Contatos.Areas.HelpPage.ModelDescriptions;
+
+namespace WebApi2_Contatos.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
